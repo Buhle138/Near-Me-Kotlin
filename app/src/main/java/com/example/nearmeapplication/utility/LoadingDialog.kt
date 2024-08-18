b90f9380ef15c46f58ffe14a4fc48169667ae7c1
buhleradzilani <radzilanibuhle@gmail.com>
2024-08-18 22:24:42 +0200

    adding the nav menu for the hamburger menu navigation.

---
+++ app/src/main/java/com/example/nearmeapplication/utility/LoadingDialog.kt
@@ -0,0 +1,15 @@
+package com.example.nearmeapplication.utility
+
+import android.app.Activity
+import android.app.AlertDialog
+import com.example.nearmeapplication.R
+
+class LoadingDialog(private  val activity: Activity) {
+
+    private var alertDialog: AlertDialog?= null
+
+//    fun startLoading() {
+//        val builder=AlertDialog.Builder(activity, R.style.)
+//    }
+
+}
