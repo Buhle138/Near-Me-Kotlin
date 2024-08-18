af12edc76582b6df6d9e4e259436b3b3f7706abc
buhleradzilani <radzilanibuhle@gmail.com>
2024-08-18 20:47:01 +0200

    Adding the necessary dependencies to the application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+   
+
 }
 
 android {
@@ -37,6 +39,7 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
