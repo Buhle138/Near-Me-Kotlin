f0cb7e7a5b5debd0b22535f3aad4707d3dfdeec1
buhleradzilani <radzilanibuhle@gmail.com>
2024-08-22 23:08:35 +0200

    changes to the home fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("androidx.databinding:databinding-common:8.5.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
